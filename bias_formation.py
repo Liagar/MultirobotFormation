c27b3e1b6efa89103028ff7c96088294a477d8e6
Liagar <liagar@gmail.com>
2024-02-27 19:21:06 +0100

    Arreglados algunos fallos pero hay que revisar todo bien

--- bias_formation.py
+++ bias_formation.py
@@ -13,13 +13,14 @@ import graph_utils as graf
 #import time
 import robot_plot_utils as rpu
 
-#TODO: Revisar. Creo que hay un problema con la coordenada y
+#TODO: Revisar muy bien las matrices y dimensiones. 
 # Posiciones iniciales de los agentes
 #pi=np.array([[-5 ,0],[-5, 5],[5, 5],[5, 0]])
 pi=np.array([[0,0],[0, 5],[5, 0]],dtype=np.float64)
 nagentes,dim=np.shape(pi)
 
 # Grafo de conexiones
+#TODO: Ver, al ser no dirigido necesito duplicarlo
 Z=np.array([[1,2],[1,3]])
 narcos,m=np.shape(Z)
 
@@ -34,8 +35,8 @@ B= graf.matriz_incidencia(Z, nagentes, narcos)
 print(np.transpose(B))
 
 # Posiciones relativas objetivo
-zstar=np.array([[0, -6],[-6, 0]])
-zstar_s=np.zeros([narcos*2,1])
+zstar=np.array([[0, -5],[0, 5],[-5, 0],[5, 0]])
+zstar_s=np.zeros([narcos*4,1])
 zstar_s=graf.stack(zstar)
 
 dt=0.1
@@ -59,37 +60,42 @@ I2=np.ones([dim,dim])
 #print(I2)
 Bbar=np.kron(B,I2)
 Bbar=np.transpose(Bbar)
-z=np.zeros([narcos*m,1])
+z=np.zeros([narcos*m*2,1])
 k=0
 Im=np.ones([dim*nagentes,1])
 
-Bias=np.zeros([nagentes,nagentes])
-#Bias[0,1]=0.5
+Bias=np.array([2,0,2,0,0,0,0,0]) #El robot 1 mete un error de 0.5 
 BiasBar=np.kron(Bias,I2)
 BiasBar=np.transpose(BiasBar)
-K=0.11
+#print(BiasBar)
+K=0.5
 p=np.zeros([nagentes,2],dtype=np.float64)
 p=pi
-M=np.array([[1,1],[-1,0],[0,-1]])
+#TODO Revisar la matriz de incidencia M
+M=np.array([[-1,1,0],[1,-1,0],[-1,0,1],[1,0,-1]])
 Mbar=np.kron(M,I2)
 
 posiciones_us=np.zeros([nagentes,nptos+1])
 pos_s=np.zeros([nagentes,2])
 pos_us=np.zeros([2*nagentes,1])
-nptos=3
+
 #Integrador
 for k in range(1,nptos):
     l=0
     pos_us[:,0]=posiciones[:,k-1]
     posiciones_us=graf.unstack(pos_us, 2)
-    print("posiciones_us")
-    print(posiciones_us)
+    #print("posiciones_us")
+    #print(posiciones_us)
     for i in range(narcos):
         head=Z[i,0]
         tail=Z[i,1]
         z[l,0]=posiciones_us[head-1,0]-posiciones_us[tail-1,0]
         l+=1
         z[l,0]=posiciones_us[head-1,1]-posiciones_us[tail-1,1]
+        l+=1
+        z[l,0]=-z[l-2,0]
+        l+=1
+        z[l,0]=-z[l-2,0]
         l+=1
        
     #print(np.shape(z)) 
@@ -102,26 +108,24 @@ for k in range(1,nptos):
     #M1=np.dot(Bbar,zdif)
     #print(np.shape(M1))
     #pdot=-K*(M1+np.dot(BiasBar,Im))
-    pdot=-K*np.dot(Mbar,zdif)
-    print("pdot")
-    print(pdot)
+    pdot=-K*np.dot(np.transpose(Mbar),zdif)
+    #print("pdot")
+    #print(pdot)
     l=0
-    #TODO Revisar por que razon p es un array de enteros!!!!
     for j in range(nagentes):
         xdot,ydot,thetadot=rob.integrator_point_model(pdot[l],pdot[l+1])
         p[j,0]+=(float(dt*xdot[0]))
         p[j,1]+=dt*ydot[0]
         l+=2
-        print("p")
-        print(p)
+        #print("p")
+        #print(p)
 
     p_s=graf.stack(p)
-    print("pstack")
-    print(p_s)
     #print(np.shape(p_s))
     posiciones[:,k]=p_s[:,0]
     t+=dt
-
+print("pstack")
+print(p_s)
     
 colors_f=['bo','go','ro','co','yo','mo','ko','bo','go']
 colors_i=['bx','gx','rx','cx','yx','mx','kx','bx','gx']
@@ -129,12 +133,29 @@ colors_l=['b:','g:','r:','c:','y:','m','k','b:','g:']
 
 j=0
 pf=np.zeros([nagentes,2])
+error=np.zeros([8])
+error[0]=posiciones[0,nptos-1]-posiciones[2,nptos-1]
+error[1]=posiciones[1,nptos-1]-posiciones[3,nptos-1]
+error[2]=-error[0]
+error[3]=-error[1]
+error[4]=posiciones[0,nptos-1]-posiciones[4,nptos-1]
+error[5]=posiciones[1,nptos-1]-posiciones[5,nptos-1]
+error[6]=-error[4]
+error[7]=-error[6]
+#TODO: Por que tengo un error de 2.5???
+print("zfinal")
+print(error)
+print("zdiff(real)")
+print(zstar_s[:,0]-np.transpose(error))
+print("zdiff_medido")
+print(zstar_s[:,0]-np.transpose(error)+Bias)
+
 for i in range(nagentes):
     plt.plot(posiciones[j,0],posiciones[j+1,0],colors_i[i])
     plt.plot(posiciones[j,nptos-1],posiciones[j+1,nptos-1],colors_f[i])
-    plt.plot(posiciones[j,:],posiciones[j+1,:],colors_l[i])
+    plt.plot(posiciones[j,0:nptos-1],posiciones[j+1,0:nptos-1],colors_l[i])
     pf[i,0]=posiciones[j,nptos-1]
-    pf[i,1]=posiciones[j,nptos-1]
+    pf[i,1]=posiciones[j+1,nptos-1]
     j=j+2
 rpu.dibuja_grafo_2D(pf,Z)
 plt.grid()
