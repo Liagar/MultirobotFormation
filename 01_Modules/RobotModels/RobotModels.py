e8ceae21e8357326868dde9c8b45daf5a38a78b0
Liagar <liagar@gmail.com>
2025-02-05 11:05:42 +0100

    Organizacion de codigo e incorporacion de código multiagente de Juan

---
+++ 01_Modules/RobotModels/RobotModels.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Lia Garcia-Perez
+Common models of mobile robots
+"""
+from math import cos, sin, tan
+
+def integrator_point_model(v,w):
+    xdot=v
+    ydot=w
+    thetadot=0
+    sdot=[xdot,ydot,thetadot]
+    return sdot
+
+
+def unicycle(v,w,theta):
+    xdot=v*cos(theta)
+    ydot=v*sin(theta)
+    thetadot=w
+    sdot=[xdot,ydot,thetadot]
+    return sdot
+
+def bicycle(v,w,theta,gamma,B):
+    xdot = v*cos(theta)
+    ydot=v*sin(theta)
+    try:
+        thetadot=(v/B)*tan(gamma)
+    except ZeroDivisionError:
+        thetadot=0
+    gammadot=w
+    sdot=[xdot,ydot,thetadot,gammadot]
+    return sdot
+
+def extended_unicycle(v,w,theta,a):
+    xdot=v*cos(theta)-a*w*sin(theta)
+    ydot=v*sin(theta)+a*w*cos(theta)
+    thetadot=w
+    sdot=[xdot,ydot,thetadot]
+    return sdot    
